21f754523fa5f04965de89e9cff416e50586b768
Moshe Shaham <ishener@gmail.com>
2013-07-15 14:01:39 +0300

    added preset

--- war/app.js
+++ war/app.js
@@ -33,11 +33,14 @@ app.filter('filterThem', function () {
 app.controller("MainCtrl", function($scope, $http, localStorageService) {
 	$scope.items = [];
 	$scope.filtered = localStorageService.get('hnfilter');
+	if ($scope.filtered == null)  { 
+		$scope.filtered = [];
+		if (QueryString.preset != undefined) 
+			$scope.filtered.push([QueryString.preset, 0]);
+		
+	}
 	$scope.cursor;
 	$scope.scrollLoad = false;
-	if ($scope.filtered == null) $scope.filtered = [];
-	
-	
 	
 	$scope.clicked = function(event, word) {
 		$scope.filtered.push([sanitizeWord(word), 0]);
@@ -83,3 +86,26 @@ app.directive('ngRightClick', function($parse) {
 function sanitizeWord(word) {
 	return word.replace(/(^[^a-z0-9\s]*)|([^a-z0-9\s]*$)|(('s)$)/gi, '')
 }
+
+var QueryString = function () {
+	  // This function is anonymous, is executed immediately and 
+	  // the return value is assigned to QueryString!
+	  var query_string = {};
+	  var query = window.location.hash.substring(1);
+	  var vars = query.split("&");
+	  for (var i=0;i<vars.length;i++) {
+	    var pair = vars[i].split("=");
+	    	// If first entry with this name
+	    if (typeof query_string[pair[0]] === "undefined") {
+	      query_string[pair[0]] = pair[1];
+	    	// If second entry with this name
+	    } else if (typeof query_string[pair[0]] === "string") {
+	      var arr = [ query_string[pair[0]], pair[1] ];
+	      query_string[pair[0]] = arr;
+	    	// If third or later entry with this name
+	    } else {
+	      query_string[pair[0]].push(pair[1]);
+	    }
+	  } 
+	    return query_string;
+} ();
